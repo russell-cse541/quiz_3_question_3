09ba809caf4d1f0426931c21f240c437fb818bb1
Nathan Russell <nathan@blackpathway.com>
2024-04-01 18:21:10 -0400

    cleaned for review

--- matrix_test.c
+++ matrix_test.c
@@ -18,11 +18,13 @@
 int main() {
 
 #ifdef USE_MY_MALLOC
-    initialize_memory_pool(40000*40000);
+    size_t memory_pool_size = 8000*8000; //just large enough for test with seed 123
+    initialize_memory_pool(memory_pool_size); 
 #endif
     int seed = 123;
     srand(seed);
-    int testCount = 1;
+    int testCount = 10;
+    // int testCount = 1;
     int modValue = 1000;
     int offsetValue = 500;
     int *randomSizes = (int*)MALLOC(testCount * sizeof(int));
@@ -34,8 +36,8 @@ int main() {
         randomSizes[i] = rand() % modValue + offsetValue;
     }
     for (int i=0; i<testCount; i++) {
-        // int size = randomSizes[i];
-        int size = 4000;
+        int size = randomSizes[i];
+        // int size = 4000;
         matrixProducts[i] = buildMatrixTest(size);
         verifySuccess(matrixProducts[i]);
         if (matrixProducts[i] != NULL && matrixProducts[i]->success) {
@@ -43,16 +45,17 @@ int main() {
         } else {
             printf("Test Failed for size %d\n", size);
         }
-        if (i >= 2) {
+        if (i >= 2) { // This offsets/delays the deallocation to add more fragmentation
             destroyMatrixProduct(matrixProducts[i-2]);
         }
     }
 
-    if (testCount >= 2) {
-        for (int i = testCount - 2; i < testCount; i++) {
+    if (testCount < 2) { // This is to ensure deallocation for small number of test cases
+        for (int i = 0; i < testCount; i++) {
             destroyMatrixProduct(matrixProducts[i]);
         }
     }
+    
 
     FREE(randomSizes);
     FREE(matrixProducts);
